b87fed87b03a1df084f411a355c9dff33962620a
Yago de Andrade <ayav@ic.ufal.br>
2021-11-25 12:25:46 -0300

    sidemenu component added

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Finance</title>
+
+    <!-- <link rel="icon" href="img/favicon.png" type="image/png"> -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- themefy CSS -->
+    <link rel="stylesheet" href="vendors/themefy_icon/themify-icons.css" />
+    <!-- swiper slider CSS -->
+    <link rel="stylesheet" href="vendors/swiper_slider/css/swiper.min.css" />
+    <!-- select2 CSS -->
+    <link rel="stylesheet" href="vendors/select2/css/select2.min.css" />
+    <!-- select2 CSS -->
+    <link rel="stylesheet" href="vendors/niceselect/css/nice-select.css" />
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="vendors/owl_carousel/css/owl.carousel.css" />
+    <!-- gijgo css -->
+    <link rel="stylesheet" href="vendors/gijgo/gijgo.min.css" />
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="vendors/font_awesome/css/all.min.css" />
+    <link rel="stylesheet" href="vendors/tagsinput/tagsinput.css" />
+    <!-- datatable CSS -->
+    <link rel="stylesheet" href="vendors/datatable/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" href="vendors/datatable/css/responsive.dataTables.min.css" />
+    <link rel="stylesheet" href="vendors/datatable/css/buttons.dataTables.min.css" />
+    <!-- text editor css -->
+    <link rel="stylesheet" href="vendors/text_editor/summernote-bs4.css" />
+    <!-- morris css -->
+    <link rel="stylesheet" href="vendors/morris/morris.css">
+    <!-- metarial icon css -->
+    <link rel="stylesheet" href="vendors/material_icon/material-icons.css" />
+
+    <!-- menu css  -->
+    <link rel="stylesheet" href="css/metisMenu.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/colors/default.css" id="colorSkinCSS">
+</head>
+
+<body class="crm_body_bg">
+
+
+
+    <!-- main content part here -->
+
+    <!-- sidebar  -->
+    <?php include 'side-menu.php' ?>
+    <!--/ sidebar  -->
+
+
+    <section class="main_content dashboard_part">
+
+        <div class="main_content_iner ">
+            <div class="container-fluid plr_30 body_white_bg pt_30">
+                <div class="row justify-content-center">
+                    <div class="col-md-6 col-lg-6 col-xl-3">
+                        <div class="white_box min_430">
+                            <div class="box_header  box_header_block">
+                                <div class="main-title">
+                                    <h3 class="mb-0">% of Expenses Budget</h3>
+                                </div>
+                            </div>
+                            <div id="radial_1"></div>
+                            <div class="radial_footer">
+                                <div class="radial_footer_inner d-flex justify-content-between">
+                                    <div class="left_footer">
+                                        <h5> <span style="background-color: #EDECFE;"></span> Blance</h5>
+                                        <p>-$18,570</p>
+                                    </div>
+                                    <div class="left_footer">
+                                        <h5> <span style="background-color: #A4A1FB;"></span> Blance</h5>
+                                        <p>$31,430</p>
+                                    </div>
+                                </div>
+                                <div class="radial_bottom">
+                                    <p><a href="#">View Full Report</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-3 ">
+                        <div class="white_box mb_30 min_430">
+                            <div class="box_header  box_header_block">
+                                <div class="main-title">
+                                    <h3 class="mb-0">% of Income Budget</h3>
+                                </div>
+                            </div>
+                            <div id="radial_2"></div>
+                            <div class="radial_footer">
+                                <div class="radial_footer_inner d-flex justify-content-between">
+                                    <div class="left_footer">
+                                        <h5> <span style="background-color: #EDECFE;"></span> Blance</h5>
+                                        <p>-$18,570</p>
+                                    </div>
+                                    <div class="left_footer">
+                                        <h5> <span style="background-color: #A4A1FB;"></span> Blance</h5>
+                                        <p>$31,430</p>
+                                    </div>
+                                </div>
+                                <div class="radial_bottom">
+                                    <p><a href="#">View Full Report</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-3 ">
+                        <div class="white_box mb_30 min_430">
+                            <div class="box_header  box_header_block">
+                                <div class="main-title">
+                                    <h3 class="mb-0">% of Income Budget</h3>
+                                </div>
+                            </div>
+                            <div id="radial_3"></div>
+                            <div class="radial_footer">
+                                <div class="radial_footer_inner d-flex justify-content-between">
+                                    <div class="left_footer">
+                                        <h5> <span style="background-color: #EDECFE;"></span> Blance</h5>
+                                        <p>-$18,570</p>
+                                    </div>
+                                    <div class="left_footer">
+                                        <h5> <span style="background-color: #A4A1FB;"></span> Blance</h5>
+                                        <p>$31,430</p>
+                                    </div>
+                                </div>
+                                <div class="radial_bottom">
+                                    <p><a href="#">View Full Report</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-3 ">
+                        <div class="white_box mb_30 min_430">
+                            <div class="box_header  box_header_block">
+                                <div class="main-title">
+                                    <h3 class="mb-0">% of Income Budget</h3>
+                                </div>
+                            </div>
+                            <div id="radial_4"></div>
+                            <div class="radial_footer">
+                                <div class="radial_footer_inner d-flex justify-content-between">
+                                    <div class="left_footer">
+                                        <h5> <span style="background-color: #EDECFE;"></span> Blance</h5>
+                                        <p>-$18,570</p>
+                                    </div>
+                                    <div class="left_footer">
+                                        <h5> <span style="background-color: #A4A1FB;"></span> Blance</h5>
+                                        <p>$31,430</p>
+                                    </div>
+                                </div>
+                                <div class="radial_bottom">
+                                    <p><a href="#">View Full Report</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="white_box mb_30">
+                            <div class="box_header ">
+                                <div class="main-title">
+                                    <h3 class="mb-0">Area Chart</h3>
+                                </div>
+                            </div>
+                            <canvas id="highlights"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- footer part -->
+        <div class="footer_part">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="footer_iner text-center">
+                            <p>2020 © Influence - Designed by <a href="#"> <i class="ti-heart"></i> </a><a href="#"> Dashboard</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- main content part end -->
+
+    <!-- footer  -->
+    <!-- jquery slim -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <!-- popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- bootstarp js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- sidebar menu  -->
+    <script src="js/metisMenu.js"></script>
+    <!-- waypoints js -->
+    <script src="vendors/count_up/jquery.waypoints.min.js"></script>
+    <!-- waypoints js -->
+    <script src="vendors/chartlist/Chart.min.js"></script>
+    <!-- counterup js -->
+    <script src="vendors/count_up/jquery.counterup.min.js"></script>
+    <!-- swiper slider js -->
+    <script src="vendors/swiper_slider/js/swiper.min.js"></script>
+    <!-- nice select -->
+    <script src="vendors/niceselect/js/jquery.nice-select.min.js"></script>
+    <!-- owl carousel -->
+    <script src="vendors/owl_carousel/js/owl.carousel.min.js"></script>
+    <!-- gijgo css -->
+    <script src="vendors/gijgo/gijgo.min.js"></script>
+    <!-- responsive table -->
+    <script src="vendors/datatable/js/jquery.dataTables.min.js"></script>
+    <script src="vendors/datatable/js/dataTables.responsive.min.js"></script>
+    <script src="vendors/datatable/js/dataTables.buttons.min.js"></script>
+    <script src="vendors/datatable/js/buttons.flash.min.js"></script>
+    <script src="vendors/datatable/js/jszip.min.js"></script>
+    <script src="vendors/datatable/js/pdfmake.min.js"></script>
+    <script src="vendors/datatable/js/vfs_fonts.js"></script>
+    <script src="vendors/datatable/js/buttons.html5.min.js"></script>
+    <script src="vendors/datatable/js/buttons.print.min.js"></script>
+
+    <script src="js/chart.min.js"></script>
+    <!-- progressbar js -->
+    <script src="vendors/progressbar/jquery.barfiller.js"></script>
+    <!-- tag input -->
+    <script src="vendors/tagsinput/tagsinput.js"></script>
+    <!-- text editor js -->
+    <script src="vendors/text_editor/summernote-bs4.js"></script>
+
+    <script src="vendors/apex_chart/apexcharts.js"></script>
+
+    <!-- custom js -->
+    <script src="js/custom.js"></script>
+
+    <!-- active_chart js -->
+    <script src="js/active_chart.js"></script>
+    <script src="vendors/apex_chart/radial_active.js"></script>
+    <script src="vendors/apex_chart/stackbar.js"></script>
+    <script src="vendors/apex_chart/area_chart.js"></script>
+    <!-- <script src="vendors/apex_chart/pie.js"></script> -->
+    <script src="vendors/chartjs/chartjs_init.js"></script>
+    <script src="vendors/apex_chart/bar_active_1.js"></script>
+    <script src="vendors/chartjs/chartjs_active.js"></script>
+
+</body>
+
+</html>
